1ec6c9bc15227979786093ae5e947ba1af0bac2a
Waqar <waqaryazdani2@gmail.com>
2020-11-21 16:49:38 +0500

    Database Design In Progress Phase 2

---
+++ database/migrations/2020_11_21_114725_create_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->double('credit_affiliate','customer_point');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
